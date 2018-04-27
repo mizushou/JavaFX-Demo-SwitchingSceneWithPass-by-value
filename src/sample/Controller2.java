bad3202f1e1c46cf22607a59592a399a0bd3e1b4
Shouhei.Mizuno <32non23@gmail.com>
2018-04-26 21:16:07 -0700

    make layout2.fxml and Controller2.java

---
+++ src/sample/Controller2.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controller2 {
+
+    private Stage thisWindow;
+
+    private Scene prevScnen;
+    private Scene thisScnen;
+    private Scene nextScnen;
+
+    public void setThisWindow(Stage thisWindow) {
+        this.thisWindow = thisWindow;
+    }
+
+    public void setPrevScnen(Scene prevScnen) {
+        this.prevScnen = prevScnen;
+    }
+
+    public void setThisScnen(Scene thisScnen) {
+        this.thisScnen = thisScnen;
+    }
+
+    public void setNextScnen(Scene nextScnen) {
+        this.nextScnen = nextScnen;
+    }
+
+    @FXML
+    private void setScene(ActionEvent event) {
+        thisWindow.setScene(prevScnen);
+    }
+
+}
